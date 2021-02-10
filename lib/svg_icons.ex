ebb9f37c7751b9c9d866f0195c21ca453463c4d3
Lloyd Ramey <lloyd.ramey@bti360.com>
2021-02-09 22:43:27 -0500

    clean up and add a simple test for the svg icon macro

--- lib/svg_icons.ex
+++ lib/svg_icons.ex
@@ -1,18 +1,210 @@
 defmodule SvgIcons do
   @moduledoc """
-  Documentation for `SvgIcons`.
+  This module is used to Handle creating modules for SVG icons using macros to insert the
+  icons at compile time. This allows using inline svgs without having to maintain the svgs
+  inline.
   """
+  defmacro __using__(opts) do
+    path_parts = Keyword.get(opts, :path)
+    extension = Keyword.get(opts, :ext, ".svg")
+    path_sep = Keyword.get(opts, :path_sep, "/")
+    base_dir = Keyword.get(opts, :base_dir, Path.dirname(__CALLER__.file))
+    include_surface = Keyword.get(opts, :surface, true)
 
-  @doc """
-  Hello world.
+    quote do
+      @path_pattern SvgIcons.collect_pattern_parts(unquote(path_parts))
 
-  ## Examples
+      @svgs SvgIcons.read_svgs(
+              unquote(base_dir),
+              @path_pattern,
+              unquote(extension),
+              unquote(path_sep)
+            )
 
-      iex> SvgIcons.hello()
-      :world
+      defp svgs(), do: @svgs
 
-  """
-  def hello do
-    :world
+      with {:module, _} <- Code.ensure_compiled(Surface) do
+        unquote(if include_surface, do: define_surface_macro())
+      end
+
+      defmacro svg(id, attrs \\ []) do
+        {:safe, render_svg(id, attrs)}
+      end
+
+      def render_svg(id, attrs), do: SvgIcons.render_svg(svgs(), id, attrs)
+    end
+  end
+
+  def render_svg(svgs, id, attrs) do
+    if Map.has_key?(svgs, id) do
+      [head, tail] = Map.get(svgs, id)
+
+      [head, translate_attrs(attrs), tail]
+    else
+      IO.warn("Could not find icon for #{inspect(id)}")
+      ["<span>", "Failed to load icon ", inspect(id), "</span>"]
+    end
+  end
+
+  def define_surface_macro() do
+    quote do
+      use Surface.MacroComponent
+
+      for {name, _, _, default} <- @path_pattern do
+        quote do
+          prop(unquote({name, [], Elixir}), :string, default: unquote(default))
+        end
+      end
+
+      prop(id, :string)
+      prop(class, :string)
+      prop(opts, :keyword, default: [])
+
+      def expand(attributes, _children, meta),
+        do: SvgIcons.expand(__MODULE__, svgs(), attributes, meta, @path_pattern)
+    end
+  end
+
+  defmacro read_svgs(base_dir, pattern_parts, extension, path_sep) do
+    quote do
+      SvgIcons.read_svgs(
+        __MODULE__,
+        unquote(base_dir),
+        unquote(pattern_parts),
+        unquote(extension),
+        unquote(path_sep)
+      )
+    end
+  end
+
+  def expand(module, svgs, attributes, meta, path_pattern) do
+    with {:module, _module} <- Code.ensure_compiled(Surface.MacroComponent) do
+      props = Surface.MacroComponent.eval_static_props!(module, attributes, meta.caller)
+
+      defaults =
+        for {name, _, _, default} <- path_pattern, into: %{} do
+          {name, default}
+        end
+
+      capture_names =
+        path_pattern
+        |> Enum.map(fn {name, _, _, _} -> name end)
+        |> Enum.reject(&is_nil/1)
+
+      id =
+        capture_names
+        |> Enum.map(fn name -> props[name] || Map.get(defaults, name) end)
+        |> List.to_tuple()
+
+      class = props[:class] || ""
+      opts = props[:opts] || []
+
+      attrs =
+        opts ++
+          [class: class] ++
+          Enum.map(capture_names, fn name ->
+            {"data-#{to_string(name)}", props[name]}
+          end)
+
+      struct!(Surface.AST.Literal, value: render_svg(svgs, id, attrs) |> IO.iodata_to_binary())
+    end
+  end
+
+  def read_svgs(module, base_dir, pattern_parts, extension, path_sep) do
+    regex =
+      ((pattern_parts
+        |> Enum.map(fn {_, pattern, _, _} -> pattern end)
+        |> Enum.join(path_sep)) <> Regex.escape(extension))
+      |> Regex.compile!()
+
+    capture_names =
+      pattern_parts
+      |> Enum.map(fn {name, _, _, _} -> name end)
+      |> Enum.reject(&is_nil/1)
+
+    capture_name_strings = Enum.map(capture_names, &to_string/1)
+
+    files =
+      ((pattern_parts
+        |> Enum.map(fn {_, _, wildcard, _} -> wildcard end)
+        |> Enum.join(path_sep)) <> extension)
+      |> Path.expand(base_dir)
+      |> Path.wildcard()
+      |> Enum.sort()
+
+    for path <- files,
+        relative_path = Path.relative_to(path, base_dir),
+        captures = Regex.named_captures(regex, relative_path),
+        captures != nil,
+        id =
+          capture_name_strings
+          |> Enum.map(fn name -> captures[name] end)
+          |> List.to_tuple(),
+        into: %{} do
+      Module.put_attribute(module, :external_resource, Path.relative_to_cwd(path))
+
+      "<svg" <> contents =
+        path
+        |> File.read!()
+        |> String.replace("\n", "")
+        |> String.trim()
+
+      {id, ["<svg", contents]}
+    end
+  end
+
+  def collect_pattern_parts(path_parts) do
+    Enum.map(path_parts, fn
+      path when is_binary(path) ->
+        path_segment(path)
+
+      name when is_atom(name) ->
+        named_path_segment(name)
+
+      {name, default} when is_atom(name) and is_binary(default) ->
+        named_path_segment(name, default)
+
+      {name, options} when is_atom(name) and is_list(options) ->
+        enum_path_segment(name, options)
+
+      {name, options, default} when is_atom(name) and is_list(options) and is_binary(default) ->
+        enum_path_segment(name, options, default)
+    end)
+  end
+
+  defp path_segment(path), do: {nil, Regex.escape(path), path, nil}
+
+  defp named_path_segment(name, default \\ nil),
+    do: {name, "(?<#{to_string(name)}>[^/]+)", "*", default}
+
+  defp enum_path_segment(name, values, default \\ nil) do
+    regex_or =
+      values
+      |> Enum.map(&to_string/1)
+      |> Enum.map(&Regex.escape/1)
+      |> Enum.join("|")
+
+    wildcard_or =
+      values
+      |> Enum.map(&to_string/1)
+      |> Enum.join(",")
+
+    {name, "(?<#{to_string(name)}>#{regex_or})", "{#{wildcard_or}}", default}
+  end
+
+  defp translate_attrs([]) do
+    []
+  end
+
+  defp translate_attrs([{key, true} | tail]) do
+    [" ", to_string(key), translate_attrs(tail)]
+  end
+
+  defp translate_attrs([{_, value} | tail]) when is_nil(value) or value == false do
+    translate_attrs(tail)
+  end
+
+  defp translate_attrs([{key, value} | tail]) do
+    [" ", to_string(key), ~S(="), value, ~S("), translate_attrs(tail)]
   end
 end
--- test/svg_icons_test.exs
+++ test/svg_icons_test.exs
@@ -2,7 +2,21 @@ defmodule SvgIconsTest do
   use ExUnit.Case
   doctest SvgIcons
 
+  defmodule HeroIcon do
+    use SvgIcons,
+      surface: false,
+      path: ["support/test-icons/optimized", {:variant, [:outline, :solid], "outline"}, :icon]
+  end
+
   test "greets the world" do
-    assert SvgIcons.hello() == :world
+    require HeroIcon
+
+    assert {:safe, value} = HeroIcon.svg({"outline", "adjustments"})
+
+    assert to_string(value) == """
+           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">\
+             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"/>\
+           </svg>\
+           """
   end
 end
